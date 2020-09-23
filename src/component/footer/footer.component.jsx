ba5ddb652a4a9dfba88ccb3a7626536c7fbeca26
aemperador <emperadoragustin@gmail.com>
2020-09-22 23:28:01 -0300

    footer arreglado

--- src/component/footer/footer.component.jsx
+++ src/component/footer/footer.component.jsx
@@ -5,9 +5,11 @@ const Footer = () => {
 
     return (
         <div className='footer'>
+            <div className='grid-container'>
             <div className='grid'>
                 <div className='col'>Contacto</div>
                 <div className='col'>Acerca de</div>
+            </div>
             </div>
             <div className='copyright'>
                 <div className='text' >
